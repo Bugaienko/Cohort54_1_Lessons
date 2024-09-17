c72e289006b9991dfc22d2ad65a626f3cecd6bd1
SergeyB <sergiibugaienko@gmail.com>
2024-09-17 22:11:23 +0200

    lesson 03 add

---
+++ src/homework_02/HomeWork02.java
@@ -0,0 +1,21 @@
+package homework_02;
+
+/**
+ * @author Sergey Bugaenko
+ * {@code @date} 17.09.2024
+ */
+
+/*
+#  <  >  +  - ,  .  :  ; (  ) {  }  /  \
+ */
+
+public class HomeWork02 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        // "Java" - экранирования
+        System.out.println("\"Java\"");
+        System.out.println("\\");
+
+//        # < > + - , . : ; ( ) { } / \
+    }
+}
