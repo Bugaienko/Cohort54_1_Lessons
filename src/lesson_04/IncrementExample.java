e85d6602087c8d95dd07109d5f7c7fc6c9dc992c
SergeyB <sergiibugaienko@gmail.com>
2024-09-20 08:11:43 +0200

    lesson 04 add

---
+++ src/lesson_04/IncrementExample.java
@@ -0,0 +1,45 @@
+package lesson_04;
+
+/**
+ * @author Sergey Bugaenko
+ * {@code @date} 19.09.2024
+ */
+
+public class IncrementExample {
+    public static void main(String[] args) {
+
+        int var = 5;
+
+        var = var + 10;
+        System.out.println("var: " + var);
+
+        var += 2; // var = var + 2;
+        System.out.println("var + : " + var);
+
+        var -= 2; // var = var - 2;
+        System.out.println("var - : " + var);
+
+        var *= 2; //var = var * 2
+
+        var /= 2; // var = var / 2
+
+        System.out.println("var: " + var);
+
+        // Инкремент, декремент
+        var++; // var = var + 1; var += 1; // Увеличить текущее значение переменной на 1
+        System.out.println(var);
+        var--; // var = var - 1; var -= 1; Уменьшить текущее значение переменной на 1
+
+        //
+        System.out.println(" ==================== ");
+        int x = 100;
+        System.out.println("x: " + x++);
+        System.out.println("x: " + x);
+
+        //
+        int y = 100;
+        System.out.println("y: " + ++y);
+        System.out.println("y: " + y);
+
+    }
+}
